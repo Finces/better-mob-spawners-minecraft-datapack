abf43a3fdf0a6d915aaaf286a89d6bb345c177f6
Jacek Tokarz <jacektokarz3@gmail.com>
2025-06-11 16:17:42 +0200

    excluded ds_store

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -r better-mob-spawners.zip ./ -x "*.git*" "readme_images" "README.md" "spyglass.json" "better-mob-spawners.zip" "build.sh"
+zip -r better-mob-spawners.zip ./ -x "*.git*" "readme_images" "README.md" "spyglass.json" "better-mob-spawners.zip" "build.sh" ".DS_Store"
